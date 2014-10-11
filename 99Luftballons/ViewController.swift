a92e83a0aa7bfad79d5df849d1e76db8d8972956
Dan <danmanteufel@yahoo.com>
2014-10-11 15:47:56 -0500

    This is the basic elements of the program.  The random images are not yet hooked up to the balloons.

--- 99Luftballons/ViewController.swift
+++ 99Luftballons/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonImageView: UIImageView!
+    @IBOutlet weak var balloonNumberLabel: UILabel!
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    
+    @IBOutlet weak var temporaryTestLabel: UILabel!
+    
+    var luftballons: [Balloon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+            
+        create99Luftballons()
+        displayLuftballon()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func create99Luftballons() {
+        for counter in 1...99 {
+            luftballons.append(Balloon(orderNumber: counter,randomImageName: "Test\(counter)"))
+        }
     }
+    
+    func displayLuftballon() {
+        balloonNumberLabel.text = "\(luftballons.last!.orderNumber)"
+        temporaryTestLabel.text = luftballons.last!.randomImageName
+    }
+    
+    @IBAction func nextButtonPressed(sender: UIBarButtonItem) {
+        if luftballons.count > 1 {
+            luftballons.removeLast()
+            displayLuftballon()
+        } else {
+            balloonNumberLabel.font = UIFont.systemFontOfSize(48.0)
+            balloonNumberLabel.text = "All Gone!"
+            temporaryTestLabel.text = "Done"
+            nextButton.enabled = false
+            nextButton.title = "Done"
+        }
+    }
+    
+}
 
-
+struct Balloon {
+    let orderNumber: Int
+    let randomImageName: String
 }
 
