c733a64bbc39032a4caf949532cb736f295398a1
Dan <danmanteufel@yahoo.com>
2014-10-11 16:07:59 -0500

    Images are now displayed and the temporary test label is removed.

--- 99Luftballons/ViewController.swift
+++ 99Luftballons/ViewController.swift
@@ -14,9 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var balloonNumberLabel: UILabel!
     @IBOutlet weak var nextButton: UIBarButtonItem!
     
-    @IBOutlet weak var temporaryTestLabel: UILabel!
+//    @IBOutlet weak var temporaryTestLabel: UILabel!
     
     var luftballons: [Balloon] = []
+    var currentIndex = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +29,37 @@ class ViewController: UIViewController {
 
     func create99Luftballons() {
         for counter in 1...99 {
-            luftballons.append(Balloon(orderNumber: counter,randomImageName: "Test\(counter)"))
+            luftballons.append(Balloon(orderNumber: counter,randomImageName: assignRandomImage()))
         }
     }
     
     func displayLuftballon() {
         balloonNumberLabel.text = "\(luftballons.last!.orderNumber)"
-        temporaryTestLabel.text = luftballons.last!.randomImageName
+//        temporaryTestLabel.text = luftballons.last!.randomImageName
+        balloonImageView.image = UIImage(named: luftballons.last!.randomImageName)
+    }
+    
+    func assignRandomImage() -> String {
+        var randomIndex: Int
+        
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(4)))
+        } while currentIndex == randomIndex
+
+        currentIndex = randomIndex
+        
+        switch randomIndex {
+        case 0:
+            return "RedBalloon1.jpg"
+        case 1:
+            return "RedBalloon2.jpg"
+        case 2:
+            return "RedBalloon3.jpg"
+        case 3:
+            return "RedBalloon4.jpg"
+        default:
+            return "BerlinTVTower.jpg"
+        }
     }
     
     @IBAction func nextButtonPressed(sender: UIBarButtonItem) {
@@ -44,7 +69,7 @@ class ViewController: UIViewController {
         } else {
             balloonNumberLabel.font = UIFont.systemFontOfSize(48.0)
             balloonNumberLabel.text = "All Gone!"
-            temporaryTestLabel.text = "Done"
+//            temporaryTestLabel.text = "Done"
             nextButton.enabled = false
             nextButton.title = "Done"
         }
